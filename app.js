5c44d801fdc4238e9a0b00c80c9b8de43ec284b1
Daniel Andrade <danielqa17@gmail.com>
2021-05-07 10:26:59 -0300

    Other3

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express'),
   bodyParser = require('body-parser'),
-  path = require('path') 
+  path = require('path');
+//var jsforce        = require('jsforce');
 var app = express();
 var crypto = require("crypto");
 var consumerSecretApp = process.env.CANVAS_CONSUMER_SECRET || '7F1D62DFA69EFAA7086979BE0D9E0D21CB21AD8F93D6494A21812CC61781203B';
@@ -17,8 +18,25 @@ app.use(bodyParser.json());
  
 
 app.get('/', function (req, res) {
-  //res.render('hello');
-  res.render('index', {title: '', req: null});
+  /*var conn = new jsforce.Connection({
+    // you can change loginUrl to connect to sandbox or prerelease env.
+    // loginUrl : 'https://test.salesforce.com'
+  });
+  var username = 'yourusername';
+  var password = 'yourpassword+securitytoken';
+  conn.login(username, password, function(err, userInfo) {
+    if (err) { return console.error(err); }
+    // Now you can get the access token and instance URL information.
+    // Save them to establish connection next time.
+    console.log(conn.accessToken);
+    console.log(conn.instanceUrl);
+    // logged in user property
+    console.log("User ID: " + userInfo.id);
+    console.log("Org ID: " + userInfo.organizationId);
+    // ...
+    */
+    //res.render('hello');
+    res.render('index', {title: '', req: null});
 });
 
 app.post('/', function (req, res) { 
