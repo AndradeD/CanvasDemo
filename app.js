bbb07a79ebe0985c3ff7c41d6ebbf5958a6f17a6
AndradeD <danielqa17@gmail.com>
2021-06-02 17:11:21 -0300

    Update app.js

--- app.js
+++ app.js
@@ -18,26 +18,6 @@ app.use(bodyParser.json());
 var cases = []
 
 app.get('/', function (req, res) {
-  /*var conn = new jsforce.Connection({
-    // you can change loginUrl to connect to sandbox or prerelease env.
-    // loginUrl : 'https://test.salesforce.com'
-  });
-  var username = 'yourusername';
-  var password = 'yourpassword+securitytoken';
-  conn.login(username, password, function(err, userInfo) {
-    if (err) { return console.error(err); }
-    // Now you can get the access token and instance URL information.
-    // Save them to establish connection next time.
-    console.log(conn.accessToken);
-    console.log(conn.instanceUrl);
-    // logged in user property
-    console.log("User ID: " + userInfo.id);
-    console.log("Org ID: " + userInfo.organizationId);
-    // ...
-    */
-    //res.render('hello');
-
-   // res.json({ casesFromServer: cases });
     res.render('index', {title: '', req: null, casesFromServer: JSON.stringify(cases)});
 });
 
